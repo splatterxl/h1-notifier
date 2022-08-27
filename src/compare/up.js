4b0c438bc175d5549c7a4eb06afb7276b8666743
splatterxl <splatterxl@outlook.ie>
2022-08-27 15:39:39 +0100

    Update up.js

--- src/compare/up.js
+++ src/compare/up.js
@@ -4,19 +4,19 @@
  */
 export const reputationChange = (base, now) => {
 	/**
-	 * @type {{ changes: string[] }}
+	 * @type {{ changed: string[] }}
 	 */
 	const obj = {
 		// @ts-ignore
 		name: 'change',
-		changes: []
+		changed: []
 	};
 
 	for (const hacker of base.values()) {
 		const newHacker = now.get(hacker.username);
 
 		if (!newHacker) continue;
-		else if (hacker.rep != newHacker.rep) obj.changes.push(hacker.username);
+		else if (hacker.rep != newHacker.rep) obj.changed.push(hacker.username);
 	}
 
   return obj
